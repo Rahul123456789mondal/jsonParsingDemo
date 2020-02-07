ea86fa61dc39e3aff076bded54d8d96590626889
Rahul123456789mondal <arkamondal1995@outlook.com>
2020-02-07 16:56:33 +0530

    Initial commit - 07/02/2020

---
+++ app/src/main/java/com/example/jsonparsingdemo/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.jsonparsingdemo
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONException
+import org.json.JSONObject
+
+class MainActivity : AppCompatActivity() {
+    val TAG = "MyActivity"
+    private val JSON_STRING : String = "{\"Arka\":{\"id\":\"1\",\"2name\":\"Mondal\",\"address\":{\"village\":\"Palla Road\"}}}"
+    var name : String = ""
+    var salary : String = ""
+
+    lateinit var employeeName : TextView
+    lateinit var employeeSalary : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // get the reference of TextView's
+        employeeName = findViewById(R.id.name)
+        employeeSalary = findViewById(R.id.salary)
+
+        try {
+            // get JSONObject from JSON file
+            var obj : JSONObject = JSONObject(JSON_STRING)
+            // fetch JSONObject name employee
+            var arka : JSONObject = obj.getJSONObject("Arka")
+            var address : JSONObject = arka.getJSONObject("address")
+            // get employee name and salary
+            name = address.getString("village")
+            //Log.d(TAG,"JSON DATA : -"+name)
+            //salary = employee.getString("salary")
+            // set emploee name and salary in TextView's
+            employeeName.text = "Id : "+name
+            //employeeSalary.text = "Salary : "+salary
+
+        }catch ( e: JSONException ){
+            Log.d(TAG, "Error : ${e.message}")
+            e.printStackTrace()
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

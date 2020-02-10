86323e7cb3ad9c91559b8dcdacdf71af4b627c96
Rahul123456789mondal <arkamondal1995@outlook.com>
2020-02-10 14:12:29 +0530

    05:03PM - 10/02/2020

--- app/src/main/java/com/example/jsonparsingdemo/MainActivity.kt
+++ app/src/main/java/com/example/jsonparsingdemo/MainActivity.kt
@@ -6,8 +6,11 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONException
 import org.json.JSONObject
+import java.io.IOException
+import java.nio.charset.Charset
 
 class MainActivity : AppCompatActivity() {
+    private var jsonDATA = ArrayList<String>()
     val TAG = "MyActivity"
     private val JSON_STRING : String = "{\"Arka\":{\"id\":\"1\",\"2name\":\"Mondal\",\"address\":{\"village\":\"Palla Road\"}}}"
     var aDDress : String = ""
@@ -15,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var employeeName : TextView
     lateinit var employeeSalary : TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,22 +29,57 @@ class MainActivity : AppCompatActivity() {
 
         try {
             // get JSONObject from JSON file
-            var obj : JSONObject = JSONObject(JSON_STRING)
+                var obj : JSONObject = JSONObject(JSON_STRING) // in this line read the json from the string
+                val jasonData = JSONObject(loadJSONFromAsset())
             // fetch JSONObject name employee
-            var arka : JSONObject = obj.getJSONObject("Arka")
-            var address : JSONObject = arka.getJSONObject("address")
-            // get employee name and salary
-            aDDress = address.getString("village")
-            //Log.d(TAG,"JSON DATA : -"+name)
-            //salary = employee.getString("salary")
-            // set emploee name and salary in TextView's
-            employeeName.text = "Id : "+aDDress
-            //employeeSalary.text = "Salary : "+salary
+                val arka : JSONObject = obj.getJSONObject("Arka")
+                val address : JSONObject = arka.getJSONObject("address")
+
+                val subassemblies : JSONObject = jasonData.getJSONObject("subassemblies")
+                val componentAnalyzer1 : JSONObject = subassemblies.getJSONObject("componentAnalyzer1")
+                val collectors : JSONObject = componentAnalyzer1.getJSONObject("collectors")
+                val bp1 : JSONObject = collectors.getJSONObject("bp1")
+                val CSV  = bp1.getJSONArray("CSV")
+            // implement for loop for getting JSON data
+                for (i in 0 until CSV.length()){
+                    val userDetail = CSV.getJSONArray(i)
+                    for (j in 0 until userDetail.length()){
+                        val example = userDetail[j]
+                        Log.d(TAG, "Data is : - "+example)
+                        employeeSalary.text = "Data : - "+example
+                    }
+                }
+
+            // get employee name and salary & In this step the data will show into the view
+                aDDress = address.getString("village")
+                //Log.d(TAG,"JSON DATA : -"+name)
+                //salary = employee.getString("salary")
+                // set emploee name and salary in TextView's
+                employeeName.text = "Id : "+aDDress
+                //employeeSalary.text = "Salary : "+salary
 
         }catch ( e: JSONException ){
             Log.d(TAG, "Error : ${e.message}")
             e.printStackTrace()
         }
+    }
+
+    private fun loadJSONFromAsset(): String{
+        //function to load the JSON from the Asset and return the object
+        var json: String = null.toString()
+        val charset: Charset = Charsets.UTF_8
+
+        try {
+            val jsonFileLoad = assets.open("demo.json")
+            val size = jsonFileLoad.available()
+            val buffer = ByteArray(size)
+            jsonFileLoad.read(buffer)
+            jsonFileLoad.close()
+            json = String(buffer, charset)
+        } catch (ex : IOException){
+            ex.printStackTrace()
+        }
+        return json
     }
 }
 
@@ -70,7 +109,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
+//Log.d(TAG,"Acess the node : -"+subassemblies)
 
 
 
